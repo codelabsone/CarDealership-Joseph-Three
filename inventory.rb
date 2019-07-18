84c29b0213cc58927bf37bd130a85650d8c99bcc
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-18 18:30:55 -0500

    Vehicle class model

--- inventory.rb
+++ inventory.rb
@@ -7,3 +7,30 @@ puts Time.new
 end
 puts "car fun"
 puts "car inventory"
+
+
+class Vehicle
+
+  def initialize(brand,model,year,price=0)
+    @brand = brand.capitalize
+    @model = model.capitalize
+    @year = year
+    @price = price
+  end
+
+  def to_s
+    "#{@brand} #{@model} made in #{@year} that costs $#{@price}."
+  end
+
+  def dent
+    @price -= 500
+  end
+
+  def scratch
+    @price -= 250
+  end
+
+end
+
+vehicle1 = Vehicle.new("chevrolet","corvette",2019,125000)
+puts vehicle1
