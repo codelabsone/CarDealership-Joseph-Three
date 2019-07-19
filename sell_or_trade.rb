2ba39c2354de27b61b1a639241670ef33713000a
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-18 20:07:40 -0500

    updated sell_or_trade

--- sell_or_trade.rb
+++ sell_or_trade.rb
@@ -1,10 +1,23 @@
 class Appraisal
+attr_writer :brand,:model,:color,:year,:mileage
 
   def initialize(brand,model,color,year,mileage)
+    @brand = brand.capitalize
+    @model = model.capitalize
+    @color = color
+    @year = year
+    @mileage = mileage
+  end
 
+  def price
+    price = @year.to_i + @mileage.to_i
   end
 
   def to_s
+    "Your #{@year} #{@color} #{@brand} #{@model} with #{@mileage} miles is worth $#{price}."
   end
 
 end
+
+car1 = Appraisal.new("chevrolet","corvette","red","2019","50000")
+puts car1
