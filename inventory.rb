a5d189de80d2ef6b5e0e2027574b4f8551a85044
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-18 19:05:47 -0500

    vin number baseline to string

--- inventory.rb
+++ inventory.rb
@@ -1,6 +1,6 @@
 class Vehicle
 
-  def initialize(brand,model,color,year,price=0,vin=00000000000000000)
+  def initialize(brand,model,color,year,price=0,vin="00000000000000000")
     @brand = brand.capitalize
     @model = model.capitalize
     @color = color
@@ -23,9 +23,11 @@ class Vehicle
 
 end
 
-vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,125000,12345678901234567)
-vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,125000,12345678901234567)
-vehicle2.scratch
+vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567")
+vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567")
+vehicle3 = Vehicle.new("ford","F-150","blue",2013,34000,"321598764TU785RC6P8")
+vehicle4 =
 
 puts vehicle1
 puts vehicle2
+puts vehicle3
