2cf3779d6df9e07f4990e892f8117b13702d47d2
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-18 19:38:17 -0500

    added mileage to inventory

--- inventory.rb
+++ inventory.rb
@@ -1,17 +1,18 @@
 class Vehicle
 attr_reader :vin
 
-  def initialize(brand,model,color,year,price=0,vin="00000000000000000")
+  def initialize(brand,model,color,year,price=0,vin="00000000000000000",mileage)
     @brand = brand.capitalize
     @model = model.capitalize
     @color = color
     @year = year
     @price = price
     @vin = vin
+    @mileage = mileage
   end
 
   def to_s
-    "A #{@color} #{@brand} #{@model} made in #{@year} that costs $#{@price} with a vin of #{@vin}. \nThe vin number is #{@vin.length} characters long."
+    "A #{@color} #{@brand} #{@model} made in #{@year} with #{@mileage} miles that costs $#{@price} with a vin of #{@vin}. \nThe vin number is #{@vin.length} characters long."
   end
 
   def dent
@@ -24,11 +25,11 @@ attr_reader :vin
 
 end
 
-vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567")
-vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567")
-vehicle3 = Vehicle.new("ford","F-150","blue",2013,34000,"321598764TU785RC6")
-vehicle4 = Vehicle.new("dodge","stratus","red",2006,900,"4NHTU892449856145")
-vehicle5 = Vehicle.new("dodge","charger","silver",2016,13000,"3JW85DH84FBY8732F")
+vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567",50000)
+vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567",4500)
+vehicle3 = Vehicle.new("ford","F-150","blue",2013,34000,"321598764TU785RC6",2361)
+vehicle4 = Vehicle.new("dodge","stratus","red",2006,900,"4NHTU892449856145",38756)
+vehicle5 = Vehicle.new("dodge","charger","silver",2016,13000,"3JW85DH84FBY8732F",692684)
 puts vehicle1
 puts vehicle2
 puts vehicle3
