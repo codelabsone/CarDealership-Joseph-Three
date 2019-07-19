fdf533ae34269e1dbcc871b444513ff998bb72f0
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-18 20:14:38 -0500

    added condition

--- sell_or_trade.rb
+++ sell_or_trade.rb
@@ -1,12 +1,13 @@
 class Appraisal
-attr_writer :brand,:model,:color,:year,:mileage
+attr_writer :brand,:model,:color,:year,:mileage,:condition
 
-  def initialize(brand,model,color,year,mileage)
+  def initialize(brand,model,color,year,mileage,condition)
     @brand = brand.capitalize
     @model = model.capitalize
     @color = color
     @year = year
     @mileage = mileage
+    @condition = condition
   end
 
   def price
@@ -14,10 +15,10 @@ attr_writer :brand,:model,:color,:year,:mileage
   end
 
   def to_s
-    "Your #{@year} #{@color} #{@brand} #{@model} with #{@mileage} miles is worth $#{price}."
+    "Your #{@year} #{@color} #{@brand} #{@model} with #{@mileage} miles in #{@condition} condition is worth $#{price}."
   end
 
 end
 
-car1 = Appraisal.new("chevrolet","corvette","red","2019","50000")
+car1 = Appraisal.new("chevrolet","corvette","red","2019","50000","very good")
 puts car1
