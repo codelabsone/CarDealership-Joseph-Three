3737839363911edd52cb7ab684e2a3f1b4d866fa
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-22 19:46:48 -0500

    added condition

--- inventory.rb
+++ inventory.rb
@@ -25,11 +25,11 @@ attr_reader :vin
 
 end
 
-vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,"125,000","12345678901234567","50,000")
-vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,"125,000","12345678901234567","4,500")
-vehicle3 = Vehicle.new("ford","F-150","blue",2013,"34,000","321598764TU785RC6","2,361")
-vehicle4 = Vehicle.new("dodge","stratus","red",2006,900,"4NHTU892449856145","38,756")
-vehicle5 = Vehicle.new("dodge","charger","silver",2016,"13,000","3JW85DH84FBY8732F","692,684")
+vehicle1 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567",50000)
+vehicle2 = Vehicle.new("chevrolet","corvette","red",2019,125000,"12345678901234567",4500)
+vehicle3 = Vehicle.new("ford","F-150","blue",2013,34000,"321598764TU785RC6",2361)
+vehicle4 = Vehicle.new("dodge","stratus","red",2006,900,"4NHTU892449856145",38756)
+vehicle5 = Vehicle.new("dodge","charger","silver",2016,13000,"3JW85DH84FBY8732F",692684)
 
 vehicles = [vehicle1,vehicle2,vehicle3,vehicle4,vehicle5]
 puts vehicles
--- sell_or_trade.rb
+++ sell_or_trade.rb
@@ -10,15 +10,42 @@ attr_writer :brand,:model,:color,:year,:mileage,:condition
     @condition = condition
   end
 
-  def price
-    price = @year.to_i + @mileage.to_i
+  def condition
+    if @condition == "very good"
+      0.90
+
+    elsif @condition == "good"
+      0.70
+
+    elsif @condition == "ok"
+      0.60
+
+    elsif @condition == "bad"
+      0.40
+
+    else @condition == "very bad"
+      0.20
+    end
+  end
+
+  def est_value
+    (@year.to_i + @mileage.to_i) * condition
   end
 
   def to_s
-    "Your #{@year} #{@color} #{@brand} #{@model} with #{@mileage} miles in #{@condition} condition is worth $#{price}."
+    "Your vehicle is valued at $#{est_value}."
   end
 
 end
 
-car1 = Appraisal.new("chevrolet","corvette","red","2019","50,000","very good")
+
+car1 = Appraisal.new("chevrolet","corvette","red",2019,50000,"very good")
+car2 = Appraisal.new("chevrolet","corvette","red",2019,125000,"good")
+car3 = Appraisal.new("ford","F-150","blue",2013,34000,"ok")
+car4 = Appraisal.new("dodge","stratus","red",2006,900,"bad")
+car5 = Appraisal.new("dodge","charger","silver",2016,13000,"very bad")
 puts car1
+puts car2
+puts car3
+puts car4
+puts car5
