d2c72cad80294f1b0fd0d691b851eb385addabdd
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-07-25 17:32:33 -0500

    changed vehilce class in financials to car

--- financials.rb
+++ financials.rb
@@ -1,5 +1,5 @@
 #Profit per sale
-class Vehicle
+class Car
 
   def initialize(vin=00000000000000000,buy_price,sell_price,list_price)
     @vin = vin
@@ -18,11 +18,11 @@ class Vehicle
 
 end
 
-vehicle1 = Vehicle.new("12345678901234567",2435,3457,"125,000")
-vehicle2 = Vehicle.new("12345678901234567",1325,4657,"125,000")
-vehicle3 = Vehicle.new("321598764TU785RC6",23552,46574,"34,000")
-vehicle4 = Vehicle.new("4NHTU892449856145",2352,46753,900)
-vehicle5 = Vehicle.new("3JW85DH84FBY8732F",123534,12454,"13,000")
+vehicle1 = Car.new("12345678901234567",2435,3457,"125,000")
+vehicle2 = Car.new("12345678901234567",1325,4657,"125,000")
+vehicle3 = Car.new("321598764TU785RC6",23552,46574,"34,000")
+vehicle4 = Car.new("4NHTU892449856145",2352,46753,900)
+vehicle5 = Car.new("3JW85DH84FBY8732F",123534,12454,"13,000")
 
 profit = [vehicle1,vehicle2,vehicle3,vehicle4,vehicle5]
 puts profit
