026d60e43eaaff303432fc3c2fa22adce4b10c6f
Chris Palmergit config --global user.email cwpalmer1s@yahoo.com <cwpalmer1s@yahoo.com>
2019-08-01 18:24:40 -0500

    financials changes for net profit

--- financials.rb
+++ financials.rb
@@ -8,11 +8,11 @@ class Financials
   end
 
   def net_profit
-    @net_profit = Randomizer.random_sell_price - Randomizer.random_buy_price
+    Randomizer.random_sell_price - Randomizer.random_buy_price
   end
 
   def to_s
-    "#{Randomizer.random_vin} was listed at $#{Randomizer.random_list_price}.  #{Randomizer.random_vin} was bought for $#{Randomizer.random_buy_price} and sold for $#{Randomizer.random_sell_price}.  The net profit for #{Randomizer.random_vin} was $#{@net_profit}."
+    "#{Randomizer.random_vin} was listed at $#{Randomizer.random_list_price}.  #{Randomizer.random_vin} was bought for $#{Randomizer.random_buy_price} and sold for $#{Randomizer.random_sell_price}.  The net profit for #{Randomizer.random_vin} was $#{net_profit}."
   end
 end
 
